1e9dc3c5a1c63c38cab0cb1dd6b76649efc7b481
azermu-milk <1390973247@qq.com>
2019-12-25 17:19:17 +0800

    create myblogserver

---
+++ myblogserver/src/test/java/com/example/myblogserver/MyblogserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myblogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
